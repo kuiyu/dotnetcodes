5451a995258ebf25964ebc09c0fb0e683ae82d3e
fangqm <79599287@qq.com>
2015-05-06 19:13:36 +0800

    增加了模拟Get,Post的方法

--- DotNet.Utilities/网络/HttpHelper.cs
+++ DotNet.Utilities/网络/HttpHelper.cs
@@ -18,6 +18,79 @@ namespace DotNet.Utilities
     /// </summary>
     public class HttpHelper
     {
+        #region 模拟GET
+        /// <summary>
+        /// GET请求
+        /// </summary>
+        /// <param name="Url">The URL.</param>
+        /// <param name="postDataStr">The post data string.</param>
+        /// <returns>System.String.</returns>
+        public static string HttpGet(string Url, string postDataStr)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(Url + (postDataStr == "" ? "" : "?") + postDataStr);
+            request.Method = "GET";
+            request.ContentType = "text/html;charset=UTF-8";
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Stream myResponseStream = response.GetResponseStream();
+            StreamReader myStreamReader = new StreamReader(myResponseStream, Encoding.GetEncoding("utf-8"));
+            string retString = myStreamReader.ReadToEnd();
+            myStreamReader.Close();
+            myResponseStream.Close();
+
+            return retString;
+        }
+        #endregion
+
+        #region 模拟POST
+        /// <summary>
+        /// POST请求
+        /// </summary>
+        /// <param name="posturl">The posturl.</param>
+        /// <param name="postData">The post data.</param>
+        /// <returns>System.String.</returns>
+        public static string HttpPost(string posturl, string postData)
+        {
+            Stream outstream = null;
+            Stream instream = null;
+            StreamReader sr = null;
+            HttpWebResponse response = null;
+            HttpWebRequest request = null;
+            Encoding encoding = System.Text.Encoding.GetEncoding("utf-8");
+            byte[] data = encoding.GetBytes(postData);
+            // 准备请求...
+            try
+            {
+                // 设置参数
+                request = WebRequest.Create(posturl) as HttpWebRequest;
+                CookieContainer cookieContainer = new CookieContainer();
+                request.CookieContainer = cookieContainer;
+                request.AllowAutoRedirect = true;
+                request.Method = "POST";
+                request.ContentType = "application/x-www-form-urlencoded";
+                request.ContentLength = data.Length;
+                outstream = request.GetRequestStream();
+                outstream.Write(data, 0, data.Length);
+                outstream.Close();
+                //发送请求并获取相应回应数据
+                response = request.GetResponse() as HttpWebResponse;
+                //直到request.GetResponse()程序才开始向目标网页发送Post请求
+                instream = response.GetResponseStream();
+                sr = new StreamReader(instream, encoding);
+                //返回结果网页（html）代码
+                string content = sr.ReadToEnd();
+                string err = string.Empty;
+                return content;
+            }
+            catch (Exception ex)
+            {
+                string err = ex.Message;
+                return string.Empty;
+            }
+        }
+        #endregion
+
+
         #region 预定义方法或者变更
         //默认的编码
         private Encoding encoding = Encoding.Default;
