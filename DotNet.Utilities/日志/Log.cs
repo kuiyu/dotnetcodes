c44faa4520f028bb589884a5e343208549a78016
fangqm <79599287@qq.com>
2016-06-28 11:23:40 +0800

    fix log bug

--- DotNet.Utilities/日志/Log.cs
+++ DotNet.Utilities/日志/Log.cs
@@ -16,22 +16,44 @@ namespace DotNet.Utilities
     {
         public static void Debug(object message)
         {
-            LogManager.GetLogger(GetCurrentMethodFullName()).Debug(message);
+            LogManager.GetLogger(DotNet.Utilities.Log.GetCurrentMethodFullName()).Debug(message);
         }
 
         public static void Debug(object message, Exception ex)
         {
-            LogManager.GetLogger(GetCurrentMethodFullName()).Debug(message, ex);
+            LogManager.GetLogger(DotNet.Utilities.Log.GetCurrentMethodFullName()).Debug(message, ex);
         }
 
         public static void Error(object message)
         {
-            LogManager.GetLogger(GetCurrentMethodFullName()).Error(message);
+            LogManager.GetLogger(DotNet.Utilities.Log.GetCurrentMethodFullName()).Error(message);
         }
 
         public static void Error(object message, Exception exception)
         {
-            LogManager.GetLogger(GetCurrentMethodFullName()).Error(message, exception);
+            LogManager.GetLogger(DotNet.Utilities.Log.GetCurrentMethodFullName()).Error(message, exception);
+        }
+
+      
+
+        public static void Info(object message)
+        {
+            LogManager.GetLogger(DotNet.Utilities.Log.GetCurrentMethodFullName()).Info(message);
+        }
+
+        public static void Info(object message, Exception ex)
+        {
+            LogManager.GetLogger(DotNet.Utilities.Log.GetCurrentMethodFullName()).Info(message, ex);
+        }
+
+        public static void Warn(object message)
+        {
+            LogManager.GetLogger(DotNet.Utilities.Log.GetCurrentMethodFullName()).Warn(message);
+        }
+
+        public static void Warn(object message, Exception ex)
+        {
+            LogManager.GetLogger(DotNet.Utilities.Log.GetCurrentMethodFullName()).Warn(message, ex);
         }
 
         private static string GetCurrentMethodFullName()
@@ -62,26 +84,6 @@ namespace DotNet.Utilities
                 str1 = null;
             }
             return str1;
-        }
-
-        public static void Info(object message)
-        {
-            LogManager.GetLogger(GetCurrentMethodFullName()).Info(message);
-        }
-
-        public static void Info(object message, Exception ex)
-        {
-            LogManager.GetLogger(GetCurrentMethodFullName()).Info(message, ex);
-        }
-
-        public static void Warn(object message)
-        {
-            LogManager.GetLogger(GetCurrentMethodFullName()).Warn(message);
-        }
-
-        public static void Warn(object message, Exception ex)
-        {
-            LogManager.GetLogger(GetCurrentMethodFullName()).Warn(message, ex);
         }
     }
 }
